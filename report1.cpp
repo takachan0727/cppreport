42fc0887b5ca27b0b61240620b246cf27ef5c970
takachan0727 <s1611526@s.tsukuba.ac.jp>
2018-12-25 12:36:52 +0900

    1225.1

---
+++ report1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class person{
+public:
+  string name;
+public:
+  string email;
+};
+
+int main()
+{
+  person friends[3];
+
+  for(int i=0; i<3; i++){
+    cout << i + 1 << "番目の名前は？";
+    cin >> friends[i].name;
+    cout << i + 1 << "番目のアドレスは？";
+    cin >> friends[i].email;
+  }
+
+  cout << "\n名前リスト:\n";
+  for(int i=0; i<3; i++){
+    cout << friends[i].name << ":" << friends[i].email << "\n";
+  }
+}
